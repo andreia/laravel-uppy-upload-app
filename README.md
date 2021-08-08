317e93c78a5d33d0a4124276d6598a86b446a0ab
Andreia Bohner <andreiabohner@gmail.com>
2021-08-07 21:08:34 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Sample Laravel App for Multipart Uploads using Laravel, AWS S3, and Uppy
+
+This is a simple app built for demonstrate the use of the [laravel-uppy-s3-multipart-upload package](https://github.com/TappNetwork/laravel-uppy-s3-multipart-upload)
+
+## Installation
+
+Clone the repository
+```
+git clone https://github.com/andreia/laravel-uppy-upload-app.git
+```
+
+Go to app directory
+```
+cd laravel-uppy-upload-app
+```
+
+Install dependencies
+```
+composer install
+```
---
+++ resources/js/bootstrap.js
@@ -0,0 +1,42 @@
+window._ = require('lodash');
+
+/**
+ * We'll load the axios HTTP library which allows us to easily issue requests
+ * to our Laravel back-end. This library automatically handles sending the
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
+ */
+
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+require('@uppy/core/dist/style.min.css')
+require('@uppy/drag-drop/dist/style.min.css')
+require('@uppy/status-bar/dist/style.min.css')
+
+import Uppy from '@uppy/core'
+import DragDrop from '@uppy/drag-drop'
+import StatusBar from '@uppy/status-bar'
+import AwsS3Multipart from '@uppy/aws-s3-multipart'
+
+window.Uppy = Uppy
+window.DragDrop = DragDrop
+window.StatusBar = StatusBar
+window.AwsS3Multipart = AwsS3Multipart
+
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allows your team to easily build robust real-time web applications.
+ */
+
+// import Echo from 'laravel-echo';
+
+// window.Pusher = require('pusher-js');
+
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: process.env.MIX_PUSHER_APP_KEY,
+//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//     forceTLS: true
+// });
